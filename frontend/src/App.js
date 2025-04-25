dee3724f7852926fcb76926484a5c91d78a15e11
Ana Julia <anajcaetanom@gmail.com>
2025-04-25 14:58:42 -0300

    fix windows 98 library usage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import './App.css';
 import "98.css";
 
@@ -22,21 +21,4 @@ const App = () => {
   );
 };
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <div style={{ width: 350 }}>
-        <App />
-      </div>
-    </div>
-  </React.StrictMode>,
-  rootElement
-);
+export default App;
