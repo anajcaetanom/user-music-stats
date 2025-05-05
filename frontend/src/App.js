98c74eaea3ebe4d4dd55e9fa5585d2483e051e7d
Ana Júlia <anajcaetanom@gmail.com>
2025-05-05 20:46:53 -0300

    frontend post with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,114 +3,175 @@ import './App.css';
 import "98.css";
 import axios from "axios";
 
-const App = () => {
 
-  const [username, setUsername] = React.useState("");
-  const [timespan, setTimespan] = React.useState("");
-  const [category, setCategory] = React.useState("");
-  const [charts, setCharts] = React.useState([]);
+const TitleBar = () => {
+  return (
+    <div className="title-bar">
+      <div className="title-bar-text">LastFM Stats</div>
+      <div className="title-bar-controls">
+        <button aria-label="Minimize" />
+        <button aria-label="Maximize" />
+        <button aria-label="Close" />
+      </div>
+    </div>
+  )
+}
+
+const Form = ({
+  onSubmit,
+  username,
+  setUsername,
+  timespan,
+  setTimespan,
+  category,
+  setCategory,
+}) => {
 
   const changeUsername = (event) => {
     const writtenUsername = event.target.value;
     setUsername(writtenUsername);
   }
-
   const changeTimespan = (event) => {
     const selectedTimespan = event.target.id;
     setTimespan(selectedTimespan);
   }
-
   const changeCategory = (event) => {
     const selectedCategory = event.target.id;
     setCategory(selectedCategory);
   }
 
+  return (
+    <form onSubmit={onSubmit}>
+      <div style={{ paddingBottom: '8px' }} className="field-row">
+        <label htmlFor="lastfm_username">LastFM User</label>
+        <input id="lastfm_username" type="text" value={username} onChange={changeUsername} required />
+      </div>
+
+      <fieldset>
+        <legend>Timespan</legend>
+        <div className="field-row">
+          <input id="7days" type="radio" name="timespan" onChange={changeTimespan} checked={timespan === "7days"} />
+          <label htmlFor="7days">Last week</label>
+        </div>
+        <div className="field-row">
+          <input id="1month" type="radio" name="timespan" onChange={changeTimespan} checked={timespan === "1month"} required />
+          <label htmlFor="1month">Last month</label>
+        </div>
+        <div className="field-row">
+          <input id="6months" type="radio" name="timespan" onChange={changeTimespan} checked={timespan === "6months"} />
+          <label htmlFor="6months">6 months</label>
+        </div>
+        <div className="field-row">
+          <input id="12months" type="radio" name="timespan" onChange={changeTimespan} checked={timespan === "12months"} />
+          <label htmlFor="12months">Last year</label>
+        </div>
+      </fieldset>
+      <div style={{ height: '8px'}} className="spacer"></div>
+      <fieldset>
+        <legend>Category</legend>
+        <div className="field-row">
+          <input id="tracks" type="radio" name="category" onChange={changeCategory} checked={category === "tracks"} />
+          <label htmlFor="tracks">Top Tracks</label>
+        </div>
+        <div className="field-row">
+          <input id="artists" type="radio" name="category" onChange={changeCategory} checked={category === "artists"} required />
+          <label htmlFor="artists">Top Artists</label>
+        </div>
+        <div className="field-row">
+          <input id="albums" type="radio" name="category" onChange={changeCategory} checked={category === "albums"} />
+          <label htmlFor="albums">Top Albums</label>
+        </div>
+      </fieldset>
+
+      <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '8px' }}>
+        <button type="submit">
+          Generate Chart
+        </button>
+      </div>
+    </form>
+  )
+}
+
+const Charts = ({ charts }) => {
+  return (
+    <div> 
+      {charts && charts.length > 0 ? (
+        <div>
+          {charts.map((chart, index) => (
+            <div key={index}>
+              {/*TODO: handle chart rendering */}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No charts available</p>
+        </div>
+      )}
+      </div>
+  );
+};
+
+const App = () => {
+  const [showResults, setShowResults] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [username, setUsername] = React.useState("");
+  const [timespan, setTimespan] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [charts, setCharts] = React.useState([]);
+
   const generateChart = async (event) => {
     event.preventDefault();
-
+    setIsLoading(true);
     try {
-        const response = await axios.get('http://localhost:5000/generate_chart', {
-            params: {
-                username: username,
-                timespan: timespan,
-                category: category
-            }
-        });
+      const response = await axios.post('http://localhost:5000/generate_chart', {
+        username: username,
+        timespan: timespan,
+        category: category
+      });
+      setCharts(response.data);
+      console.log(response.data);
+      setShowResults(true);
+    } catch (error) {
+      if (error.response) {
+        console.error('Resposta do servidor com erro:', error.response.data);
+        console.error('Código do erro:', error.response.status);
+      } else if (error.request) {
+        console.error('A requisição foi feita, mas não houve resposta', error.request);
+      } else {
+        console.error('Erro ao configurar a requisição', error.message);
+      }
+    } finally {
+      setIsLoading(false);
     }
-
-    console.log(username, timespan, category);
-  }
-
+  };
 
   return (
     <div className="background">
       <div className="window">
-        <div className="title-bar">
-          <div className="title-bar-text">LastFM Stats</div>
-          <div className="title-bar-controls">
-            <button aria-label="Minimize" />
-            <button aria-label="Maximize" />
-            <button aria-label="Close" />
-          </div>
-        </div>
-
+        <TitleBar />
         <div className="window-body">
-
-          <form onSubmit={generateChart}>
-            <div style={{ paddingBottom: '8px' }} className="field-row">
-              <label htmlFor="lastfm_username">LastFM User</label>
-              <input id="lastfm_username" type="text" onChange={changeUsername} required />
-            </div>
-
-            <fieldset>
-              <legend>Timespan</legend>
-              <div className="field-row">
-                <input id="7days" type="radio" name="timespan" onChange={changeTimespan} />
-                <label htmlFor="7days">Last week</label>
-              </div>
-              <div className="field-row">
-                <input id="1month" type="radio" name="timespan" onChange={changeTimespan} required />
-                <label htmlFor="1month">Last month</label>
-              </div>
-              <div className="field-row">
-                <input id="6months" type="radio" name="timespan" onChange={changeTimespan} />
-                <label htmlFor="6months">6 months</label>
-              </div>
-              <div className="field-row">
-                <input id="12months" type="radio" name="timespan" onChange={changeTimespan} />
-                <label htmlFor="12months">Last year</label>
-              </div>
-            </fieldset>
-            <div style={{ height: '8px'}} class="spacer"></div>
-            <fieldset>
-              <legend>Category</legend>
-              <div className="field-row">
-                <input id="tracks" type="radio" name="category" onChange={changeCategory} />
-                <label htmlFor="tracks">Top Tracks</label>
-              </div>
-              <div className="field-row">
-                <input id="artists" type="radio" name="category" onChange={changeCategory} required />
-                <label htmlFor="artists">Top Artists</label>
-              </div>
-              <div className="field-row">
-                <input id="albums" type="radio" name="category" onChange={changeCategory} />
-                <label htmlFor="albums">Top Albums</label>
-              </div>
-            </fieldset>
-
-            <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '8px' }}>
-              <button type="submit">
-                Generate Chart
-              </button>
-            </div>
-
-          </form>
-
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : !showResults ? (
+            <Form 
+              onSubmit={generateChart}
+              username={username}
+              setUsername={setUsername}
+              timespan={timespan}
+              setTimespan={setTimespan}
+              category={category}
+              setCategory={setCategory}
+            />
+          ) : (
+            <Charts charts={charts} />
+          )}
         </div>
+
       </div>
     </div>
-    
-  );
-};
+  )};
+
+
 
 export default App;
