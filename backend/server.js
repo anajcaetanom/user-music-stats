9c9df3f6095a503d72c20fc1baa132714e5c0705
Ana Júlia <anajcaetanom@gmail.com>
2025-05-06 22:03:27 -0300

    backend start

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const LastFM = require('last-fm');
+const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
+const LASTFM_API_KEY = process.env.LASTFM_API_KEY;
 
-const lastfm = new LastFM('6afb6561ded89f457d4cfbba34329bae', { 
-    userAgent: 'LastFmApp/1.0 (https://github.com/anajcaetanom/user-music-stats)'});
+app.use(cors()); // Allow all origins (good for development)
 
-// Dropdown menu options
-app.get('/api/chartOptions', (req, res) => {
-    const chartOptions = [
-        { value: 'artists', label: 'Top Artists' },
-        { value: 'tracks', label: 'Top Tracks' },
-        { value: 'albums', label: 'Top Albums' },
-    ];
-    res.json(chartOptions);
-});
-
-app.get('/api/timeOptions', (req, res) => {
-    const timeOptions = [
-        { value: '7days', label: 'Last Week' },
-        { value: '1month', label: 'Last Month' },
-        { value: '6months', label: 'Last 6 Months' },
-        { value: '12months', label: 'Last Year' },
-    ];
-    res.json(timeOptions);
-});
+app.post('/generate_chart', async (req, res) => {
+    try {
+        const { username, timespan, category } = req.body;
+    }
+})
